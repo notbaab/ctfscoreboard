d2b402fd42cda03005833d0535a30900b204cab7
Erik Parreira <etinlb@gmail.com>
2016-05-01 14:47:47 -0700

    Added database for service level tables

--- DbOperations.py
+++ DbOperations.py
@@ -23,6 +23,7 @@ def execute_trans(conn, statement, args_tup):
         print(args_tup)
         cur.execute(statement, args_tup)
         conn.commit()
+        return True
     except Exception as e:
         print(e.message)
         print("fuck man")
@@ -35,24 +36,82 @@ def register_user(conn, username, ip):
     execute_trans(conn, "INSERT INTO users (username,ip) VALUES (?,?)", (username, ip))
 
 
-def get_all_users(db_con):
-    user_query = "SELECT * FROM USERS"
-    return query_db(user_query, db_con)
+def get_or_create_vulnerable_service(conn, user, service_name):
+    service_query = "SELECT * FROM vulnerable_services WHERE user_id = (?) AND service = (?)"
+    service = query_db(conn, service_query, args=(user["id"], service_name), one=True)
+    print("service")
+    print(service)
 
-def query_db(query, db, args=(), one=False):
-    cur = db.execute(query, args)
+    if not service:
+        insert_query = "INSERT INTO vulnerable_services (user_id, service) VALUES (?,?)"
+        success = execute_trans(conn, insert_query, (user["id"], service_name))
+        if success:
+            service = query_db(conn, service_query, args=(user["id"], service_name), one=True)
+        else:
+            raise ValueError("Can't insert " + service_name + " for user " + str(user))
+
+    return service
+
+
+def update_vulnerable_services(conn, ip, service_name, vulnerable, is_up):
+    user = user_for_ip(ip, conn)
+    print(user)
+    service = get_or_create_vulnerable_service(conn, user, service_name)
+    update_query = "UPDATE vulnerable_services set vulnerable = (?)"
+    args = [vulnerable]
+    if is_up:
+        update_query += ", uptime = (?)" + str()
+        args.append(service["uptime"] + 1)
+    else:
+        update_query += ", downtime = (?)"
+        args.append(service["downtime"] + 1)
+
+    update_query += " WHERE user_id = (?) AND service = (?)"
+    args.append(user["id"])
+    args.append(service_name)
+
+    return execute_trans(conn, update_query, args)
+
+
+def get_all_users(conn):
+    user_query = "SELECT * FROM USERS"
+    return query_db(conn, user_query)
+
+
+def query_db(conn, query, args=(), one=False):
+    cur = conn.execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
 
-def user_exists_for_ip(ip):
-    users = get_all_users(g.db)
+def user_exists_for_ip(ip, conn):
+    users = get_all_users(conn)
     matching_user = [user for user in users if user["ip"]  == ip]
     return (True if len(matching_user) else False)
+
+
+def user_for_ip(ip, conn):
+    users = get_all_users(conn)
+    matching_user = [user for user in users if user["ip"]  == ip]
+    return (matching_user[0] if len(matching_user) else None)
 
 
 def match_user_to_ip(users, ip):
     matching_user = [user for user in users if user["ip"]  == ip]
     return (matching_user[0] if len(matching_user) else None)
 
+
+if __name__ == '__main__':
+    # Self test stuff
+    print("hello")
+    conn = connect_db("self_test.db")
+    register_user(conn, "user1", "123.2.2.1")
+    register_user(conn, "user2", "123.2.2.2")
+    # Add services
+    update_vulnerable_services(conn, "123.2.2.1", "service1", True, True)
+    update_vulnerable_services(conn, "123.2.2.1", "service2", True, True)
+    update_vulnerable_services(conn, "123.2.2.1", "service3", True, False)
+    update_vulnerable_services(conn, "123.2.2.2", "service1", True, True)
+    update_vulnerable_services(conn, "123.2.2.2", "service2", False, True)
+    update_vulnerable_services(conn, "123.2.2.2", "service3", False, True)
--- schema.sql
+++ schema.sql
@@ -1,9 +1,18 @@
 drop table if exists users;
 create table users (
   id integer primary key autoincrement,
-  ip text not null,
+  ip text NOT NULL UNIQUE,
   username text not null,
   score integer DEFAULT 0
 );
 
--- INSERT INTO users (ip, username) VALUES ("120.0.0.123", "Test Guy");
+drop table if exists vulnerable_services;
+create table vulnerable_services (
+  user_id integer NOT NULL,
+  service text NOT NULL,
+  uptime integer DEFAULT 0,
+  downtime integer DEFAULT 0,
+  vulnerable boolean DEFAULT true,
+  PRIMARY KEY (user_id, service),
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
